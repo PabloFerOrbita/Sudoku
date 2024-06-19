80f4cd607c46f3370e7517d9e6fb62cba923a405
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-06-19 10:57:37 +0200

    añadido botón para generar otro sudoku con la misma dificultad en medio de la partida

--- sudoku.js
+++ sudoku.js
@@ -192,8 +192,9 @@ function imprimirSudoku() {
         $('input').removeClass('bg-primary-subtle');
     })
 
-    $('#cuerpo').append('<div id="botones" class="mt-3 mb-3 d-flex flex-row w-25 justify-content-around"><button id="resolver" class="btn btn-success">Resolver</button><button id="salir" class="btn btn-success">Cambiar Dificultad</button></div> ')
+    $('#cuerpo').append('<div id="botones" class="mt-3 mb-3 d-flex flex-row w-25 justify-content-around"><button id="resolver" class="btn btn-success">Resolver</button><button id="regenerar" class="btn btn-success">Regenerar</button><button id="salir" class="btn btn-success">Cambiar Dificultad</button></div> ')
     $('#resolver').on('click', rellenar);
+    $('#regenerar').on('click', prepararJuego);
     $('#salir').on('click', preguntar_dificultad);
     $('#cuerpo').append('<h1 id="resultado" class="text-danger-emphasis"></h1>')
 
